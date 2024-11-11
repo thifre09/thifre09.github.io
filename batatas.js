1454a411b71b2992a989b8d8a6d443b13e60f9ba
thifre09 <128859182+thifre09@users.noreply.github.com>
2024-11-11 16:24:48 -0300

    adicionado todo o resto
    adicionado todo o resto do site que eu ja tinha criado antes

---
+++ batatas.js
@@ -0,0 +1,12 @@
+function enviar() {
+    const checkboxes = document.querySelectorAll('input[name="quiz"]:checked');
+    const numMarcados = checkboxes.length; // Conta os selecionados
+    const section = document.getElementById("resultados");
+    const resultadoTexto = document.getElementById("resultadoTexto");
+    
+    // Exibe o número de caixas marcadas
+    resultadoTexto.textContent = `Você accertou ${numMarcados}/30 curiosidades.`;
+    resultadoTexto.style.textAlign = "center"
+    resultadoTexto.style.fontSize = "x-Large"
+    section.style.display = "block"; // Mostra a seção de resultados
+}
