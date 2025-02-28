3d06f60a96248b026b78800e1e7553f2290f86ba
Thiago Freitas <freitas.costa@escolar.ifrn.edu.br>
2025-02-28 11:37:30 -0300

    renomeado alguns arquivos e update no poder do css

---
+++ assets/js/poder-do-css.js
@@ -0,0 +1,32 @@
+let btn = document.getElementById("botao-css");
+let estado = true;
+let estilo = document.querySelector('link[href="assets/css/poder do css.css"]');
+
+btn.style.border = "none";
+btn.style.borderRadius = "15px";
+btn.style.fontSize = "xx-large";
+btn.style.width = "fit-content";
+btn.style.position = "fixed";
+btn.style.top = "100%";
+btn.style.left = "50%";
+btn.style.transform = "translate(-50%, -103%)";
+btn.style.backgroundColor = "red";
+btn.style.cursor = "pointer";
+btn.style.padding = "10px";
+btn.style.color = "white";
+btn.style.zIndex = "2"
+
+
+function clique() {
+    if (estado) {
+        estado = false;
+        btn.style.backgroundColor = "green";
+        btn.innerText = "Ativar CSS";
+        estilo.disabled = true;
+    } else {
+        estado = true;
+        btn.style.backgroundColor = "red";
+        btn.innerText = "Desativar CSS";
+        estilo.disabled = false;
+    }
+}
