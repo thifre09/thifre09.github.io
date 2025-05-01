b7a92924a5ecd2725e72dfdb9dc44c630e7bc5ae
thifre09 <128859182+thifre09@users.noreply.github.com>
2025-05-01 11:41:34 -0300

    update conversa

--- assets/js/conversa.js
+++ assets/js/conversa.js
@@ -2,5 +2,6 @@ let playButton = document.getElementById("jogar");
 
 playButton.addEventListener("click", () => {
     document.getElementById("menu-principal").style.display = "none";
-    document.querySelector("jogo").style.display = "flex";
+    document.querySelector("div#jogo").style.display = "flex";
 });
+
