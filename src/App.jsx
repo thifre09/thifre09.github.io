e3bf62811bc1a89e672b0c30a6d94af2b3453560
thifre09 <128859182+thifre09@users.noreply.github.com>
2026-02-08 16:12:31 -0300

    configure github pages deploy

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h2>React funcionando 🎉</h2>
+      <a href="quadrado-clicker.html">Quadrado clicker</a>
+    </div>
+  )
+}
---
+++ src/components/Link.jsx
@@ -0,0 +1,6 @@
+// Componente Link
+function Link() {
+    return <p>aaaaaaaaa</p>;
+}
+
+export default Link;
