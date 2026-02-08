5b4f700d30f1bf2c3f13dcc0ea88a56210ae1794
thifre09 <128859182+thifre09@users.noreply.github.com>
2026-02-08 18:36:56 -0300

    sim

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div>
-      <h2>React funcionando 🎉</h2>
-      <a href="quadrado-clicker.html">Quadrado clicker</a>
-      <p>olaa</p>
-    </div>
-  )
-}
--- src/components/Link.jsx
+++
@@ -1,6 +0,0 @@
-// Componente Link
-function Link() {
-    return <p>aaaaaaaaa</p>;
-}
-
-export default Link;
